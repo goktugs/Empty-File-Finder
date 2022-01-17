7034abd2ec82c8dd95070aeac54618e134aedaee
Goktug Sevgil <86847572+GoktugS@users.noreply.github.com>
2022-01-17 21:35:00 +0300

    File reading part added

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
 func main() {
+
+	// get file directory from user
+	args := os.Args[1:]
+
+	// check wrong inputs
+
+	if len(args) < 1 {
+		fmt.Println("Please provide a directory")
+		return
+	}
+
+	// check files directory
+	files, err := ioutil.ReadDir(args[0])
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	// !!! Check type of files. If you want to reach name of file you need to use methods
+	//		like file.Name()
+	for _, file := range files {
+		if file.Size() == 0 {
+			name := file.Name()
+			fmt.Println(name)
+		}
+	}
 
 }
